37fbcfad0bd61a7e9e2743f02f54a5eca97e5bc4
Maria Chira <chiramariageorgiana@gmail.com>
2021-11-12 00:10:43 +0000

    Level 500 completed

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // ---------------------------------Global Variables------------------------
 
 let allEpisodes;
+const allShows = getAllShows();
 
 // root element
 const rootElem = document.getElementById("root");
@@ -8,6 +9,9 @@ const rootElem = document.getElementById("root");
 //creating the parent div for jumbotron
 const jumbotron = document.createElement("div");
 
+//creating the go back button 
+  const goBackBtn = document.createElement("p");
+  
 //create input & select and the parent div
 let searchDiv = document.createElement("div");
 searchDiv.classList.add("searchDiv");
@@ -15,39 +19,30 @@ const searchShow = document.createElement("select");
 const searchByEpisode = document.createElement("select");
 const search = document.createElement("input");
 
-
 //create parent div for card episodes
 let episodesDiv = document.createElement("div");
+
+//create parent div for shows cards
+let showsDiv = document.createElement("div");
 
 //create number of episodes paragraph
 let numberOfEp = document.createElement("p");
 
 rootElem.appendChild(jumbotron);
+rootElem.appendChild(goBackBtn);
 rootElem.appendChild(searchDiv);
 rootElem.appendChild(numberOfEp);
 rootElem.appendChild(episodesDiv);
+rootElem.appendChild(showsDiv);
 
 
-//get data from API
-function setup() {
-  
-  fetch(`https://api.tvmaze.com/shows/82/episodes`)
-    .then((response) => {
-      if (response.status == 200) {
-        return response.json();
-      }
-      throw `${response.status} ${response.statusText}`;
-    })
-    .then((data) => {
-      allEpisodes = data;
-      makeJumbotron();
-      makeShowSelector();
-      makeEpisodeSelector(allEpisodes);
-      makeEpisodeSearch();
-      makePageForEpisodes(allEpisodes);
-      makeFooter();
-    })
-    .catch((error) => console.log(error));
+//display shows' page
+function setupShowsPage() {
+  makeJumbotron();
+  makeShowSelector();
+  makeShowSearchShow();
+  createAllShows(allShows);
+  makeFooter();
 }
 
 //creating the jumbotron
@@ -60,19 +55,30 @@ function makeJumbotron() {
   jumbotron.appendChild(mainHeading);
 }
 
+//creating the go back button 
+function makeGoBackBtn (){
+  goBackBtn.innerHTML = "☚ Go back ";
+  goBackBtn.setAttribute("id", "goBackBtn");
+
+  //add eventListener on click for goBackBtn
+  function handleBackBtn() {
+    window.location.reload();
+  }
+
+  goBackBtn.addEventListener("click", handleBackBtn);
+}
+
 
 //creating the shows' select input
-function makeShowSelector(){
+function makeShowSelector() {
   searchShow.classList.add("searchInput");
   searchDiv.appendChild(searchShow);
-  searchShow.placeholder = "Select a show";
+  searchShow.placeholder = "All shows";
 
   const initialOptionShow = document.createElement("option");
   initialOptionShow.value = "";
   initialOptionShow.innerHTML = "Select a show";
   searchShow.appendChild(initialOptionShow);
-
-  const allShows = getAllShows();
 
   allShows.forEach((show) => {
     const optionShow = document.createElement("option");
@@ -81,38 +87,17 @@ function makeShowSelector(){
     optionShow.setAttribute("value", show.name);
   });
 
-  //event handler on change for select shows - display the proper episodes for each show
+  //event handler on change for select shows -
   function handleChangeShowOption(e) {
-    let filteredShow = allShows.filter((show) => show.name === e.target.value);
-    if (e.target.value == "") {
-      episodesDiv.innerHTML = "";
-    } else {
-      fetch(`https://api.tvmaze.com/shows/${filteredShow[0].id}/episodes`)
-        .then((response) => {
-          if (response.status == 200) {
-            return response.json();
-          }
-          throw `${response.status} ${response.statusText}`;
-        })
-        .then((data) => {
-          allEpisodes = data;
-          episodesDiv.innerHTML = "";
-          searchByEpisode.innerHTML = "";
-          search.innerHTML = "";
-          makeEpisodeSelector(allEpisodes);
-          makeEpisodeSearch();
-          makePageForEpisodes(data);
-        })
-        .catch((error) => console.log(error));
-    }
+    let showElement = document.getElementById(e.target.value)
+    console.log(showElement)
+    showElement.scrollIntoView();
   }
-  searchShow.addEventListener("change", handleChangeShowOption);
+    searchShow.addEventListener("change", handleChangeShowOption);
 }
 
-
-
 //creating the episode selector
-function makeEpisodeSelector(episodes){
+function makeEpisodeSelector(episodes) {
   searchByEpisode.classList.add("searchInput");
   searchDiv.appendChild(searchByEpisode);
 
@@ -141,7 +126,6 @@ function makeEpisodeSelector(episodes){
       let filteredEpisode = allEpisodes.filter(
         (episode) => e.target.value == episode.name
       );
-      console.log("aaa", filteredEpisode);
       episodesDiv.innerHTML = "";
       numberOfEp.innerHTML = "";
       makePageForEpisodes(filteredEpisode);
@@ -150,11 +134,10 @@ function makeEpisodeSelector(episodes){
   searchByEpisode.addEventListener("change", handleChangeEpisodeOption);
 }
 
-
-
-//creating the search bar
-function makeEpisodeSearch(){
+//creating the search bar for episodes
+function makeEpisodeSearch() {
   search.setAttribute("type", "text");
+  search.value = "";
   search.setAttribute("placeholder", "Search by words");
   search.classList.add("searchInput");
   searchDiv.appendChild(search);
@@ -169,12 +152,37 @@ function makeEpisodeSearch(){
     );
     episodesDiv.innerHTML = "";
     numberOfEp.innerHTML = "";
+    showsDiv.innerHTML = "";
+    createNumberOfEpisodes(filteredEpisodes);
     makePageForEpisodes(filteredEpisodes);
   }
   search.addEventListener("keyup", handleChangeSearch);
 }
 
 
+//creating the search bar for shows
+function makeShowSearchShow() {
+  search.setAttribute("type", "text");
+  search.setAttribute("placeholder", "Search by words");
+  search.classList.add("searchInput");
+  searchDiv.appendChild(search);
+
+  //event handler on keyup - for search by words
+  function handleChangeSearchShow(e) {
+    let searchValue = e.target.value;
+    let filteredShows = allShows.filter((show) => {
+      return (
+        show.name.toLowerCase().includes(searchValue.toLowerCase()) ||
+        show.summary.toLowerCase().includes(searchValue.toLowerCase())
+      );
+    });
+    console.log(filteredShows);
+    showsDiv.innerHTML = "";
+    createAllShows(filteredShows);
+  }
+  search.addEventListener("keyup", handleChangeSearchShow);
+
+}
 
 function createNumberOfEpisodes(episodeList) {
   //number of episodes paragraph
@@ -185,6 +193,105 @@ function createNumberOfEpisodes(episodeList) {
 
   //updating the number of the displayed episodes
   numberOfEp.innerHTML = `Displaying ${episodeList.length}/${allEpisodes.length} episode(s)`;
+}
+
+function createAllShows(showsList) {
+  //iterating through the shows in order to create the cards
+
+  showsList.map((show) => {
+    let showCard = document.createElement("div");
+    showCard.classList.add("showCard");
+    showsDiv.appendChild(showCard);
+
+    let showDetails = document.createElement("div");
+    showDetails.classList.add("showDetails");
+    showCard.appendChild(showDetails);
+
+    let showImage = document.createElement("img");
+    showImage.classList.add("showImage");
+    if (show.image != null) {
+      showImage.setAttribute("src", show.image.original);
+    } else {
+      showImage.setAttribute(
+        "src",
+        "https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg"
+      );
+    }
+    showDetails.appendChild(showImage);
+
+    let showInfo = document.createElement("div");
+    showInfo.classList.add("showInfo");
+    showDetails.appendChild(showInfo);
+
+    let showInfoTitle = document.createElement("h1");
+    showInfoTitle.innerHTML = `${show.name}`;
+    showInfoTitle.classList.add(`showInfoTitle`);
+    showInfoTitle.setAttribute("id", show.name);
+    showInfoTitle.addEventListener("click", headingHandler);
+    showInfo.appendChild(showInfoTitle);
+
+    let showInfoDiv = document.createElement("div");
+    showInfoDiv.classList.add("showInfoDiv");
+    showInfo.appendChild(showInfoDiv);
+
+    let showRuntime = document.createElement("p");
+    showRuntime.innerHTML = `<span>Runtime:</span> ${show.runtime} min`;
+    showRuntime.classList.add("showInfoParagraphs");
+    showInfoDiv.appendChild(showRuntime);
+
+    let showStatus = document.createElement("p");
+    showStatus.innerHTML = `<span>Status:</span> ${show.status}`;
+    showStatus.classList.add("showInfoParagraphs");
+    showInfoDiv.appendChild(showStatus);
+
+    let showRating = document.createElement("p");
+    showRating.innerHTML = `<span>Rating:</span> ${show.rating.average}`;
+    showRating.classList.add("showInfoParagraphs");
+    showInfoDiv.appendChild(showRating);
+
+    let showGenre = document.createElement("p");
+    showGenre.innerHTML = `<span>Genres:</span> ${show.genres}`;
+    showGenre.classList.add("showInfoParagraphs");
+    showInfoDiv.appendChild(showGenre);
+
+    let showSummary = document.createElement("p");
+    showSummary.innerHTML = show.summary;
+    showSummary.classList.add("showSummary");
+    showCard.appendChild(showSummary);
+
+  });
+}
+
+//
+function headingHandler(e) {
+  let filteredShow = allShows.filter(
+    (show) => show.name === e.target.innerHTML
+  );
+  if (e.target.innerHTML == "") {
+    episodesDiv.innerHTML = "";
+  } else {
+    fetch(`https://api.tvmaze.com/shows/${filteredShow[0].id}/episodes`)
+      .then((response) => {
+        if (response.status == 200) {
+          return response.json();
+        }
+        throw `${response.status} ${response.statusText}`;
+      })
+      .then((data) => {
+        allEpisodes = data;
+        episodesDiv.innerHTML = "";
+        searchByEpisode.innerHTML = "";
+        search.innerHTML = "";
+        searchDiv.innerHTML = "";
+        showsDiv.innerHTML = "";
+        makeGoBackBtn();
+        makeEpisodeSelector(allEpisodes);
+        makeEpisodeSearch();
+        makePageForEpisodes(data);
+        window.scrollTo(0, 0);
+      })
+      .catch((error) => console.log(error));
+  }
 }
 
 function createAllEpisodes(episodeList) {
@@ -199,11 +306,14 @@ function createAllEpisodes(episodeList) {
     card.appendChild(imageContainer);
 
     let image = document.createElement("img");
-    if (episode.image !=null){
+    if (episode.image != null) {
       image.setAttribute("src", episode.image.original);
-    }else{
-      image.setAttribute("src", "https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg");
-    };
+    } else {
+      image.setAttribute(
+        "src",
+        "https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg"
+      );
+    }
     image.classList.add("image");
     imageContainer.appendChild(image);
 
@@ -281,4 +391,5 @@ function formatEpisodeAndSeason(season, episode) {
   return seasonNumber + episodeNumber;
 }
 
-window.onload = setup();
+// window.onload = setup();
+window.onload = setupShowsPage();
